07de6ca39f537f5c22786f72e997e483ab0b2f34
AJ Klopfenstein <aj.klopfenstein@gmail.com>
2015-05-10 14:25:50 -0500

    Footsteps and Animations
    Things are getting close to done...

---
+++ Assets/Scripts/PlayDollFootstep.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayDollFootstep : MonoBehaviour 
+{
+    [SerializeField] private AudioClip[] m_FootstepSounds;
+
+    private AudioSource m_AudioSource;
+
+	// Use this for initialization
+	void Start () 
+    {
+        m_AudioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+	
+	}
+
+    public void PlayFootstep()
+    {
+        int n = Random.Range(1, m_FootstepSounds.Length);
+        m_AudioSource.clip = m_FootstepSounds[n];
+        m_AudioSource.PlayOneShot(m_AudioSource.clip);
+        // move picked sound to index 0 so it's not picked next time
+        m_FootstepSounds[n] = m_FootstepSounds[0];
+        m_FootstepSounds[0] = m_AudioSource.clip;
+    }
+}
