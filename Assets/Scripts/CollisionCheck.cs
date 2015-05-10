6cc0c652461461a12b9b8e39eceb8df8439f925c
AJ Klopfenstein <aj.klopfenstein@gmail.com>
2015-05-10 16:25:47 -0500

    Final Changes
    Ye ye ye

---
+++ Assets/Scripts/CollisionCheck.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollisionCheck : MonoBehaviour 
+{
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider trigger)
+    {
+        Debug.Log(trigger.name);
+        if (trigger.name == "Dolly_RigMaybe")
+        {
+            Application.LoadLevel("Title");
+        }
+    }
+}
